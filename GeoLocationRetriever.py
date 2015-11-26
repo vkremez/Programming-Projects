d78e7a9d963e94c851cbf9b3748440fa912c98e1
Vitali's Repo <vkremez@users.noreply.github.com>
2015-11-26 01:47:56 -0500

    Create GeoLocationRetriever.py

---
+++ GeoLocationRetriever.py
@@ -0,0 +1,31 @@
+#__author = vkremez
+# This programming challenge was written for University of Michigan's programming challenge using Google geocode API.
+# The purpose of this program is to retrieve specific latitude and longitude information about any geographical location.
+
+import urllib
+import json
+
+serviceurl = 'http://maps.googleapis.com/maps/api/geocode/json?'
+
+while True:
+    address = raw_input('Enter location: ')
+    if len(address) < 1 : break
+
+    url = serviceurl + urllib.urlencode({'sensor':'false', 'address': address})
+    print 'Retrieving', url
+    uh = urllib.urlopen(url)
+    data = uh.read()
+    print 'Retrieved',len(data),'characters'
+
+    try: js = json.loads(str(data))
+    except: js = None
+    if 'status' not in js or js['status'] != 'OK':
+        print '==== Failure To Retrieve ===='
+        print data
+        continue
+
+    #print json.dumps(js, indent=4)
+
+    placeid = js["results"][0]["place_id"]
+
+    print "Place id: ", placeid
